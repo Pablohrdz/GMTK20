74d5f871d858c722a2c49218d7358e69c9959f71
Jorge Gaspar <jorge.gaspar.lira@gmail.com>
2020-07-10 17:26:27 -0700

    Created base movement and controls for the submarine

---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class Emitter : MonoBehaviour
+{
+    public KeyCode linkedKey;
+    public bool active;
+    ParticleSystem particles;
+
+    private void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    public void enableParticles(bool enable)
+    {
+        ParticleSystem.EmissionModule emission = particles.emission;
+        emission.enabled = enable;
+    }
+}
---
+++ Assets/Scripts/SubmarineController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmarineController : MonoBehaviour
+{
+    // move to each individual emitter?
+    public float emissionForce; 
+    List<Emitter> emitters;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        emitters = new List<Emitter>();
+        foreach(Transform child in transform)
+        {
+            Emitter emitter = child.GetComponent<Emitter>();
+            if (emitter != null)
+            {
+                emitters.Add(emitter);
+            }
+        }
+    }
+
+    void Update()
+    {
+        foreach (var emitter in emitters)
+        {
+            if (!emitter.active) { continue; }
+            bool holeCovered = Input.GetKey(emitter.linkedKey);
+            emitter.enableParticles(!holeCovered);
+            if (!holeCovered)
+            {
+                rb.AddForce(-emitter.transform.forward.normalized * emissionForce);
+            }
+        }
+    }
+}
