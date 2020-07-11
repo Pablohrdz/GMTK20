211647da422ca7d11955cb78d738d6d341866941
Pablo Hernandez Cepeda <pablohrdz@gmail.com>
2020-07-10 22:34:33 -0500

    Parallax
    + Added vertical & horizontal parallax
    + Added placeholder art to test parallax
    + Restructured scene
    + Added Pablo prefab

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector2 ParallaxEffectMultiplier;
+    [SerializeField]
+    private bool InfiniteHorizontal;
+    [SerializeField]
+    private bool InfiniteVertical;
+
+    private Transform CameraTransform;
+    private Vector3 LastCameraPosition;
+    private float TextureUnitSizeX;
+    private float TextureUnitSizeY;
+
+    private void Start()
+    {
+        CameraTransform = Camera.main.transform;
+        LastCameraPosition = CameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        TextureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+        TextureUnitSizeY = texture.height / sprite.pixelsPerUnit;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 deltaMovement = CameraTransform.position - LastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * ParallaxEffectMultiplier.x, deltaMovement.y * ParallaxEffectMultiplier.y);
+        LastCameraPosition = CameraTransform.position;
+
+        if (InfiniteHorizontal)
+        {
+            if (Mathf.Abs(CameraTransform.position.x - transform.position.x) >= TextureUnitSizeX)
+            {
+                float offsetPositionX = (CameraTransform.position.x - transform.position.x) % TextureUnitSizeX;
+                transform.position = new Vector3(CameraTransform.position.x + offsetPositionX, transform.position.y);
+            }
+        }
+
+        if (InfiniteVertical)
+        {
+            if (Mathf.Abs(CameraTransform.position.y - transform.position.y) >= TextureUnitSizeY)
+            {
+                float offsetPositionY = (CameraTransform.position.y - transform.position.y) % TextureUnitSizeY;
+                transform.position = new Vector3(transform.position.x, CameraTransform.position.y + offsetPositionY);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/SmoothCamera2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera2D : MonoBehaviour
+{
+
+    public float DampTime = 0.15f;
+    private Vector3 Velocity = Vector3.zero;
+    public Transform Target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target)
+        {
+            Vector3 point = GetComponent<Camera>().WorldToViewportPoint(Target.position);
+            Vector3 delta = Target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref Velocity, DampTime);
+        }
+
+    }
+}
