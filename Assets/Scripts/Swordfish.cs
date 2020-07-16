e9374ce86928542843c75501acd811f9b8edf3bb
Jorge Gaspar <jorge.gaspar.lira@gmail.com>
2020-07-15 20:39:47 -0700

    Gameplay improvements

--- Assets/Scripts/Swordfish.cs
+++ Assets/Scripts/Swordfish.cs
@@ -62,24 +62,26 @@ public class Swordfish : Enemy
                 new AudioEventArgs() { sampleId = "swordfish-swim-loop", volume = 0.8f, throttleSeconds = 2.0f }
             );
 
-            // Record original position where the fish was triggered
-            LastPlayerTransform = Player;
-
             // Instance crosshair on player position
-            InstancedCrosshair = Instantiate(CrosshairPrefab, LastPlayerTransform.position, Quaternion.identity);
+            InstancedCrosshair = Instantiate(CrosshairPrefab, Player.position, Quaternion.identity);
 
             // Change color
             //Renderer.material.color = Color.red;
-
-            // Look at target
-            transform.right = transform.position - LastPlayerTransform.position;
-
+            
             // Wait in order to telegraph action
             yield return new WaitForSeconds(TelegraphDuration);
         }
 
         // Move towards target
-        transform.position = Vector2.MoveTowards(transform.position, LastPlayerTransform.position, Speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, Player.position, Speed * Time.deltaTime);
+
+        // Look at target
+        transform.right = transform.position - Player.position;
+
+        //if (InstancedCrosshair != null)
+        //{
+        //    InstancedCrosshair.transform.position = Player.position;
+        //}
     }
 
     
